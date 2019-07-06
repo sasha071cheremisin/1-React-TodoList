63c9b5c7aaac53eb8cef3a6f7fd2fc6005f9cdf6
alex <sasha071cheremisin@mail.ru>
2019-07-06 18:54:29 +0300

    Updates

---
+++ precache-manifest.634b72d98b94fb3357711f77513e5814.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d175821b60c1b800bf0a0ae023dbddad",
+    "url": "/1-React-TodoList/index.html"
+  },
+  {
+    "revision": "82ba0fcc99c558885aa8",
+    "url": "/1-React-TodoList/static/css/main.490d184f.chunk.css"
+  },
+  {
+    "revision": "5944ba1e1b241eea543d",
+    "url": "/1-React-TodoList/static/js/2.c625f864.chunk.js"
+  },
+  {
+    "revision": "82ba0fcc99c558885aa8",
+    "url": "/1-React-TodoList/static/js/main.86e8c0ee.chunk.js"
+  },
+  {
+    "revision": "678ba51292e12f087ab3",
+    "url": "/1-React-TodoList/static/js/runtime~main.57313246.js"
+  }
+]);
